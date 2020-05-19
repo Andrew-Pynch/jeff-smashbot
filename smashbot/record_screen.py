6debac84b515ace5c2004cd1341aff66b4cc77f5
Andrew Pynch <andrewpynchbusiness@gmail.com>
2020-05-19 09:52:34 -0700

    Use scalary for data collection operations

--- smashbot/record_screen.py
+++ smashbot/record_screen.py
@@ -1,26 +1,9 @@
 import time
 import cv2
 import numpy as np
-import pyautogui
-from utils.DataCollection import DataCollection
-
-
-# def record():
-#     while True:
-#         dc = DataCollection()
-#         screen = dc.GrabScreen(region=(400, 40, 1500, 1000))
-#         last_time = time.time()
-
-#         # Resize to something a little smaller for now
-#         screen = cv2.resize(screen, (480, 270))
-
-#         cv2.imshow('view', screen)
-
-#         if cv2.waitKey(25) & 0xFF == ord('q'):
-#             cv2.destroyAllWindows()
-#             break
+from scalary import DataCollection
 
 
 if __name__ == '__main__':
     dc = DataCollection()
-    dc.Record(region=(400, 40, 1500, 1000))
+    dc.Record(region=(400, 40, 1500, 1000), resize=(480, 270))
